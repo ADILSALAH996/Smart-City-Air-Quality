a8ff1d019f07c20b0905f70c75c89f0fb1d3dbd4
Adil Salah <adilsalah996@gmail.com>
2026-03-25 16:11:49 +0530

    Refactor database.py to include load_data function
    Added load_data function to retrieve and sort data from air_quality_data table.

--- database.py
+++ database.py
@@ -1,4 +1,13 @@
 import mysql.connector
+from sqlalchemy import create_engine
+
+def load_data():
+    engine = create_engine(
+        "mysql+mysqlconnector://root:Adil@#996@localhost/iot_air_quality"
+    )
+    query = "SELECT * FROM air_quality_data ORDER BY timestamp DESC LIMIT 200"
+    df = pd.read_sql(query, engine)
+    return df.sort_values("timestamp")
 
 def connect_db():
     return mysql.connector.connect(
@@ -13,10 +22,10 @@ def insert_data(data):
     cursor = conn.cursor()
 
     query = """
-    INSERT INTO air_quality_data
-    (timestamp, aqi, pm2_5, pm10, co, no2, o3)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """
+        INSERT INTO air_quality_data
+        (timestamp, aqi, pm2_5, pm10, co, no2, o3, real_aqi)
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+        """
 
     values = (
         data["timestamp"],
@@ -25,7 +34,8 @@ def insert_data(data):
         data["pm10"],
         data["co"],
         data["no2"],
-        data["o3"]
+        data["o3"],
+        data["real_aqi"]
     )
 
     cursor.execute(query, values)
